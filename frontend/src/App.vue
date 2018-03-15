1a2e84ae9f3a6f34d8d3598968166fabc1ed7840
DeepAQ <i@aq741.com>
2018-03-15 23:54:15 +0800

    frontend: base layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,71 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Layout>
+      <Header>
+        <div class="layout-logo">
+          TrainingCollege
+        </div>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-nav">
+            <menu-item name="1">
+              <Icon type="ios-navigate"></Icon>
+              Item 1
+            </menu-item>
+            <menu-item name="2">
+              <Icon type="ios-keypad"></Icon>
+              Item 2
+            </menu-item>
+            <menu-item name="3">
+              <Icon type="ios-analytics"></Icon>
+              Item 3
+            </menu-item>
+            <menu-item name="4">
+              <Icon type="ios-paper"></Icon>
+              Item 4
+            </menu-item>
+          </div>
+        </Menu>
+      </Header>
+      <Content :style="{padding: '0 50px'}">
+        <Breadcrumb :style="{margin: '20px 0'}">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Card>
+          <div style="min-height: 300px;">
+            Content
+          </div>
+        </Card>
+      </Content>
+      <Footer>
+        &copy; 2018 DeepAQ.
+        技术构架 <a href="https://github.com/DeepAQ/Autumn" target="_blank">Autumn</a>
+      </Footer>
+    </Layout>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .ivu-layout-header {
+    display: flex;
+
+    .layout-logo {
+      color: whitesmoke;
+      font-size: 24px;
+      flex: 1;
+    }
+  }
+
+  .ivu-layout-footer {
+    text-align: center;
+  }
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import './styles/base.less'
 
 Vue.config.productionTip = false
+
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
